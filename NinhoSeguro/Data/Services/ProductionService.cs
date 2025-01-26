56fb5059473c4f94dfb9d71a4581f8e4530266ba
Afonso Sousa <a104262@alunos.uminho.pt>
2025-01-26 21:37:32 +0000

    encomendasId

--- NinhoSeguro/Data/Services/ProductionService.cs
+++ NinhoSeguro/Data/Services/ProductionService.cs
@@ -25,6 +25,59 @@ namespace LI4.Data.Services
             return "Produção iniciada com sucesso.";
         }
 
+        public async Task<(bool PodeProduzir, string Mensagem, Dictionary<int, int> ProdutosProduziveis)> VerificarMateriaisParaProducaoAsync(int encomendaId)
+        {
+            var sqlProdutosEncomenda = @"
+                SELECT ep.IdProduto, ep.Quantidade, pm.IdMaterial, pm.Quantidade AS QtdNecessaria
+                FROM Encomenda_tem_Produto ep
+                JOIN Produto_tem_Material pm ON ep.IdProduto = pm.IdProduto
+                WHERE ep.NumEncomenda = @EncId";
+
+            var produtosEncomenda = await _db.LoadData<dynamic, dynamic>(sqlProdutosEncomenda, new { EncId = encomendaId });
+
+            if (produtosEncomenda == null || produtosEncomenda.Count == 0)
+                return (false, "Nenhum produto encontrado para esta encomenda.", null);
+
+            var sqlMateriaisStock = "SELECT Id, Quantidade FROM Material";
+            var materiaisStock = await _db.LoadData<Material, dynamic>(sqlMateriaisStock, new { });
+
+            Dictionary<int, int> materiaisDisponiveis = materiaisStock.ToDictionary(m => m.Id, m => m.Quantidade);
+            Dictionary<int, int> produtosProduziveis = new();
+
+            foreach (var item in produtosEncomenda)
+            {
+                int idProduto = item.IdProduto;
+                int qtdProduto = item.Quantidade;
+                int idMaterial = item.IdMaterial;
+                int qtdNecessaria = item.QtdNecessaria * qtdProduto;
+
+                if (materiaisDisponiveis.ContainsKey(idMaterial))
+                {
+                    int qtdDisponivel = materiaisDisponiveis[idMaterial];
+
+                    if (qtdDisponivel < qtdNecessaria)
+                    {
+                        int maxProduzivel = qtdDisponivel / item.QtdNecessaria;
+                        produtosProduziveis[idProduto] = maxProduzivel;
+
+                        if (maxProduzivel == 0)
+                            return (false, $"Material insuficiente para produzir o produto {idProduto}.", produtosProduziveis);
+                    }
+                    else
+                    {
+                        produtosProduziveis[idProduto] = qtdProduto;
+                    }
+                }
+                else
+                {
+                    return (false, $"Material necessário para o produto {idProduto} não encontrado no stock.", null);
+                }
+            }
+
+            return (true, "Materiais disponíveis para produção.", produtosProduziveis);
+        }
+
+
         public async Task<string> EnviarEncomendaAsync(int encomendaId)
         {
             var sqlEnc = @"SELECT * FROM Encomenda WHERE Numero = @EncId AND Estado = 'Em produção'";
